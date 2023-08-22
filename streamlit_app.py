93d83860487ae4a95ce1f23a8502e0816b7c1196
egalanopoulos37 <82552304+egalanopoulos37@users.noreply.github.com>
2023-08-22 17:49:55 -0400

    Update streamlit_app.py
    Added lines on nutritional info for the variable fruit_choice.
    Added fruit_choice as a variable to normalized json line.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,10 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display FruityVice API response
 streamlit.header("Fruityvice Fruit Advice!")
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Clearly I'm supposed to normalize something here
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
