9941bac7abc8e472e36c7b49ee40344fbc936d5b
egalanopoulos37 <82552304+egalanopoulos37@users.noreply.github.com>
2023-08-24 02:13:18 -0400

    Update streamlit_app.py
    Added an input-based section for an additional fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What additional fruit would you like to add?')
+streamlit.write('The user entered', add_my_fruit)
