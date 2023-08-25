626705d62c117479448f023518fee7f8e25a88e2
egalanopoulos37 <82552304+egalanopoulos37@users.noreply.github.com>
2023-08-25 15:42:57 -0400

    Update streamlit_app.py
    Found the error - invalid syntax: did not close parenthesis. Corrected error.
    Corrected try-except statement.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,10 @@ try:
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalize
-                        except URLError as e:
-    streamlit.error()
+    streamlit.dataframe(fruityvice_normalize)
+except URLError as e:
+  streamlit.error()
+
   
 streamlit.write('The user entered ', fruit_choice)
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
